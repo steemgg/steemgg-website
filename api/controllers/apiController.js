834a2a689c28423757e4f921af7f5824a07f7eb2
Ye Hui <bonjovis1985@gmail.com>
2017-12-06 08:47:54 +0000

    use test to mork data,easy to dev

--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -9,67 +9,46 @@ var formidable = require('formidable'),
     request = require('request'),
     decompress = require('decompress'),
     ipfsAPI = require('ipfs-api'),
+    mysql = require('mysql'),
     sc2 = require('../lib/sc2');
 
 exports.upload = function(req, res) {
-    var api = sc2.Initialize({
-        app: config.get('steemit.sc.app'),
-        scope: config.get('steemit.sc.scope'),
-        baseURL: config.get('steemit.sc.url'),
-        callbackURL: config.get('steemit.sc.cburl')
-    });
-    if (req.session.accessToken == null && process.env.NODE_ENV === 'development') {
-        console.log('cookies');
-        api.setAccessToken(req.cookies['at']);
-    } else {
-        console.log('session');
-        api.setAccessToken(req.session.accessToken);
-    } 
-    api.me(function (err, result) {
-        var me = JSON.parse(result.body)
-        if(me.error){
-            return res.status(500).json({ error: me.error_description })
-        }
-        console.log(me.account.id);
-        var userid = me.account.id;
-        var uploadDir = config.get('steemit.app.uploadurl');
-        var form = new formidable.IncomingForm()
-        form.multiples = true
-        form.keepExtensions = true
-        form.uploadDir = uploadDir
-        form.parse(req, function(err, fields, files) {
-            if (err) return res.status(500).json({ error: err })
-                var ipfs = ipfsAPI({host: config.get('steemit.ipfs.ip'), port: config.get('steemit.ipfs.port'), protocol: 'http'});
-                if(files['file'].type == 'application/zip') {
-                    console.log('unzip file');
-                    unzipFile(files['file'].path, userid, function cb(unzips){
-                        console.log(config.get('steemit.app.gameurl')+"/"+userid+"/"+unzips[0].path);
-                       ipfs.util.addFromFs(config.get('steemit.app.gameurl')+"/"+userid+"/"+unzips[0].path, { recursive: true }, (err, result) => {
-                            if (err) { 
-                                console.log(err);
-                                return res.send();
-                            }
-                            res.status(200).json({ uploaded: true, ret: result.slice(-1) });
-                        })
-                    });
-                } else {
-                       ipfs.util.addFromFs(files['file'].path, { recursive: true }, (err, result) => {
-                            if (err) { 
-                                console.log(err);
-                                return res.send();
-                            }
-                            res.status(200).json({ uploaded: true, ret: result });
-                        })
+    var userid = req.session.user.account.id;
+    var uploadDir = config.get('steemit.app.uploadurl');
+    var form = new formidable.IncomingForm()
+    form.multiples = true
+    form.keepExtensions = true
+    form.uploadDir = uploadDir
+    form.parse(req, function(err, fields, files) {
+        if (err) return res.status(500).json({ error: err })
+        var ipfs = ipfsAPI({host: config.get('steemit.ipfs.ip'), port: config.get('steemit.ipfs.port'), protocol: 'http'});
+        if(files['file'].type == 'application/zip') {
+            console.log('unzip file');
+            unzipFile(files['file'].path, userid, function cb(unzips){
+                console.log(config.get('steemit.app.gameurl')+"/"+userid+"/"+unzips[0].path);
+                ipfs.util.addFromFs(config.get('steemit.app.gameurl')+"/"+userid+"/"+unzips[0].path, { recursive: true }, (err, result) => {
+                    if (err) { 
+                        return res.status(500).json({ error: err });
+                    }
+                    res.status(200).json({ uploaded: true, ret: result.slice(-1) });
+                })
+            });
+        } else {
+            ipfs.util.addFromFs(files['file'].path, { recursive: true }, (err, result) => {
+                if (err) { 
+                    return res.status(500).json({ error: err });
                 }
-        })
-        form.on('fileBegin', function (name, file) {
-            const fileExt = path.extname(file.name);
-            file.path = path.join(uploadDir, '/image/', `${new Date().getTime()}.${fileExt}`)
-            if(file.type == 'application/zip') {
-                file.path = path.join(uploadDir, '/zip/', `${new Date().getTime()}.zip`)
-            }
-        })
-    });
+                res.status(200).json({ uploaded: true, ret: result });
+            })
+        }
+    })
+    form.on('fileBegin', function (name, file) {
+        const fileExt = path.extname(file.name);
+        file.path = path.join(uploadDir, '/image/', `${new Date().getTime()}.${fileExt}`)
+        if(file.type == 'application/zip') {
+            file.path = path.join(uploadDir, '/zip/', `${new Date().getTime()}.zip`)
+        }
+    })
 };
 
 exports.me = function(req, res) {
@@ -79,35 +58,42 @@ exports.me = function(req, res) {
         baseURL: config.get('steemit.sc.url'),
         callbackURL: config.get('steemit.sc.cburl')
     });
-    //console.log(req);
-    if (req.session.accessToken == null && process.env.NODE_ENV === 'development') {
-        console.log(req.cookies);
-        api.setAccessToken(req.cookies['at']);
-    } else {
-        console.log(req.session.accessToken);
-        api.setAccessToken(req.session.accessToken);
-    } 
+    //if (req.session.accessToken == null && process.env.NODE_ENV === 'development') {
+    //    api.setAccessToken(req.cookies['at']);
+    //} else {
+    api.setAccessToken(req.session.accessToken);
+    //}
     api.me(function (err, result) {
-        var me = JSON.parse(result.body)
-        if(me.error){
-            return res.status(500).json({ error: me.error_description })
+        if(err != null){
+            return res.status(401).json({ error: err });
         }
-        res.status(200).json(JSON.parse(result.body));
+        req.session.user = result;
+        res.status(200).json(result);
+    });
+};
+
+exports.addGame = function(req, res, next) {
+    var game = req.body;
+    var unix = Math.round(+new Date()/1000);
+    game.userid = req.session.user.account.id;
+    game.status = 1;
+    game.createtime = unix;
+    game.updatetime = unix;
+    cmysql(function cb(con){
+        console.log('con is ok');
+        con.query('INSERT INTO games SET ?', game, (err, dbRes) => {
+            if(err) {
+                console.log(err);
+            }
+            console.log('Last insert ID:', dbRes.insertId);
+            game.id = dbRes.insertId;
+            con.end();
+            res.status(200).json(game);
+        });
     });
 };
 
 exports.index = function(req, res, next) {
-    //var username = config.get('steemit.dev.username');
-    //var password = config.get('steemit.dev.password');
-    //var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-    //var wif = steem.auth.toWif(username, password, 'posting');
-    //console.log(wif);
-    //steem.broadcast.comment(password, '', 'test', username, permlink, "test", "testbody", { tags: ['test'], app: 'steemgame/examples' }, function(err, result) {
-    //    console.log(err, result);
-    //});
-    //steem.api.getDiscussionsByCreated({'tag':'cn','limit':1}, function(err, result) {
-    //    console.log(err, result);
-    //});
     var api = sc2.Initialize({
         app: config.get('steemit.sc.app'),
         callbackURL: config.get('steemit.sc.cburl'),
@@ -118,6 +104,21 @@ exports.index = function(req, res, next) {
   res.render('index', { title: '$$$ hello! Steem Game $$$', login: url});
 };
 
+exports.logout = function(req, res, next) {
+    var api = sc2.Initialize({
+        app: config.get('steemit.sc.app'),
+        callbackURL: config.get('steemit.sc.cburl'),
+        baseURL: config.get('steemit.sc.url'),
+        scope: config.get('steemit.sc.scope')
+    });
+    api.revokeToken(function (err, res) {
+        if(err != null){
+            return res.status(401).json({ error: err })
+        }
+    });
+    res.status(200).json('ok');
+};
+
 function unzipFile(file, userid, cb) {
     var ret = decompress(file, config.get('steemit.app.gameurl')+"/"+userid,{
         filter: file => path.extname(file.path) !== '.exe'
@@ -125,5 +126,29 @@ function unzipFile(file, userid, cb) {
         cb(files);
         console.log(files);
     });
+}
+
+function cmysql(cb) {
+    const con = mysql.createConnection({
+        host: config.get('steemit.db.host'),
+        user: config.get('steemit.db.dbUser'),
+        password: config.get('steemit.db.dbPass'),
+        database: config.get('steemit.db.dbName'),
+    });
+
+    con.connect((err) => {
+        if(err){
+            console.log('Error connecting to Db');
+            return;
+        }
+        console.log('Connection established');
+    });
+    cb(con);
+
+    //con.end((err) => {
+    //    // The connection is terminated gracefully
+    //    // Ensures all previously enqueued queries are still
+    //    // before sending a COM_QUIT packet to the MySQL server.
+    //});
 }
 
--- api/routes/api.js
+++ api/routes/api.js
@@ -1,14 +1,38 @@
 'use strict';
+var fs = require('fs'),
+    config = require('config');
 
 module.exports = function(app) {
   var api  = require('../controllers/apiController');
 
-  app.post('/upload', api.upload);
-  app.post('/post', api.upload);
-  app.get('/me', api.me);
+  app.post('/upload', [userMiddleware], api.upload);
+  app.post('/post', [userMiddleware], api.upload);
+  app.post('/add', [userMiddleware], api.addGame);
+  app.get('/me', [morkMiddleware], api.me);
+  app.get('/logout', api.logout);
   app.get('/', api.index);
 
   var callback  = require('../controllers/callbackController');
   app.get('/callback', callback.auth);
 };
 
+function morkMiddleware (req, res, next) {
+    if (!req.session.accessToken && process.env.NODE_ENV === 'development' && req.cookies['at'] == 'test') {
+        fs.readFile( config.get('steemit.app.rooturl') + '/test.json', 'utf8', function (err, result) {
+            if (err) {
+                return console.log(err);
+            }
+            req.session.user = JSON.parse(result);
+            res.status(200).json(JSON.parse(result));
+        });
+        return;
+    }
+    next();
+}
+
+function userMiddleware (req, res, next) {
+    if (!req.session.user) {
+        return res.status(401).json({ error: 'must be connect steemit' });
+    }
+    next();
+}
