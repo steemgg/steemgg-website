d1d466e1072ca6134d9c56c05d84ad9aa9f4c778
Ye Hui <bonjovis1985@gmail.com>
2018-01-23 03:29:04 +0000

    add db.js & post game & add game

--- api/controllers/callbackController.js
+++ api/controllers/callbackController.js
@@ -10,6 +10,7 @@ var formidable = require('formidable'),
 
 exports.auth = function(req, res, next) {
     req.session.accessToken = req.query.access_token;
+    //console.log(req.query.refresh_token);
     res.redirect('./v1/me');
 };
 
