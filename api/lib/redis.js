8c773c78fb69e83248d59e23bdd8ed522fea7732
bonjovis <bonjovis1985@gmail.com>
2018-03-15 10:27:51 +0800

    redis lib

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -15,6 +15,9 @@ function Redis(host, port) {
     );
     this.options.get = promisify(this.options.client.get).bind(this.options.client);
     this.options.set = promisify(this.options.client.set).bind(this.options.client);
+    this.options.hmset = promisify(this.options.client.hmset).bind(this.options.client);
+    this.options.hdel = promisify(this.options.client.hdel).bind(this.options.client);
+    this.options.hgetall = promisify(this.options.client.hgetall).bind(this.options.client);
     this.options.expire = promisify(this.options.client.expire).bind(this.options.client);
 }
 
@@ -28,6 +31,18 @@ Redis.prototype.set = async function(key, value) {
 
 Redis.prototype.expire = async function(key, seconds) {
     return  await this.options.expire(key, seconds);
+}
+
+Redis.prototype.hgetall = async function(key) {
+    return  await this.options.hgetall(key);
+}
+
+Redis.prototype.hdel = async function(key) {
+    return  await this.options.hdel(key);
+}
+
+Redis.prototype.hmset = async function(key, value) {
+    return  await this.options.hmset(key, value);
 }
 
 
