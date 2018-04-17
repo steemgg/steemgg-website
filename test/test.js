5644170d8af654c4417d51043e9792d1ecf87ae7
bonjovis <bonjovis1985@gmail.com>
2018-04-17 14:09:03 +0800

    bug fix

--- test/test.js
+++ test/test.js
@@ -63,14 +63,23 @@ describe('Redis', function() {
 
 describe('DB', function() {
     before(async function() {
-        await db.execute(db.WRITE, 'delete from activities where userid= ?', [testUserId]);
-        await db.execute(db.WRITE, 'delete from comments where userid= ?', [testUserId]);
-        await db.execute(db.WRITE, 'delete from games where id= ? and userid= ?', [testGameId,testUserId]);
+        try {
+            await game.clearCache();
+            await db.execute(db.WRITE, 'delete from activities where userid= ?', [testUserId]);
+            await db.execute(db.WRITE, 'delete from comments where userid= ?', [testUserId]);
+            await db.execute(db.WRITE, 'delete from games where id= ? and userid= ?', [testGameId,testUserId]);
+        } catch (err) {
+            assert.fail('test failed');
+        }
     });
     after(async function() {
-        await db.execute(db.WRITE, 'delete from activities where userid= ?', [testUserId]);
-        await db.execute(db.WRITE, 'delete from comments where userid= ?', [testUserId]);
-        await db.execute(db.WRITE, 'delete from games where id= ? and userid= ?', [testGameId,testUserId]);
+        try {
+            await db.execute(db.WRITE, 'delete from activities where userid= ?', [testUserId]);
+            await db.execute(db.WRITE, 'delete from comments where userid= ?', [testUserId]);
+            await db.execute(db.WRITE, 'delete from games where id= ? and userid= ?', [testGameId,testUserId]);
+        } catch (err) {
+            assert.fail('test failed');
+        }
     });
     describe('#add game', function() {
         it('should return 1 ',async function() {
@@ -88,21 +97,33 @@ describe('DB', function() {
                 category:'test',
                 description:'test game for unit test'
             };
-            let dbRes = await game.addGame(gameInfo);
-            assert.equal(dbRes.affectedRows, 1);
+            try{
+                let dbRes = await game.addGame(gameInfo);
+                assert.equal(dbRes.affectedRows, 1);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#query game info', function() {
         it('should return game info ',async function() {
-            let dbRes = await game.getGameById(testGameId);
-            assert.equal(dbRes[0].id, testGameId);
+            try{
+                let dbRes = await game.getGameById(testGameId);
+                assert.equal(dbRes[0].id, testGameId);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#delete game', function() {
         it('should deleted',async function() {
-            await game.deleteGame([testGameId,testUserId]);
-            let dbRes = await game.getGameById(testGameId);
-            assert.equal(dbRes[0].status, 3);
+            try{
+                await game.deleteGame([testGameId,testUserId]);
+                let dbRes = await game.getGameById(testGameId);
+                assert.equal(dbRes[0].status, 3);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#update game', function() {
@@ -117,62 +138,99 @@ describe('DB', function() {
                 category:'test',
                 description:'test game for unit test'
             };
-            await game.updateGame([gameInfo,testGameId,testUserId]);
-            let dbRes = await game.getGameById(testGameId);
-            assert.equal(dbRes[0].gameUrl, 'modify test');
+            try{
+                await game.updateGame([gameInfo,testGameId,testUserId]);
+                let dbRes = await game.getGameById(testGameId);
+                assert.equal(dbRes[0].gameUrl, 'modify test');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#audit game', function() {
         it('should audit',async function() {
             let unix = Math.round(+new Date()/1000);
             let audit = { userid:testUserId, account:testAccount, gameid:testGameId, lastModified:unix, permlink:'http',comment:'audit', type:1};
-            await game.auditGame(audit,2);
-            let dbRes = await game.getGameById(testGameId);
-            assert.equal(dbRes[0].status, 2);
+            try{
+                await game.auditGame(audit,2);
+                let dbRes = await game.getGameById(testGameId);
+                assert.equal(dbRes[0].status, 2);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('should has comment',async function() {
-            let dbRes = await game.auditComments([testGameId]);
-            assert.equal(dbRes[0].permlink, 'http');
+            try{
+                let dbRes = await game.auditComments([testGameId]);
+                assert.equal(dbRes[0].permlink, 'http');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#report game', function() {
         it('should report',async function() {
             let unix = Math.round(+new Date()/1000);
             let report = { userid:testUserId, account:testAccount, gameid:testGameId, lastModified:unix, permlink:'report http', comment:'report'};
-            await game.reportGame(report);
-            let dbRes = await game.getGameById(testGameId);
-            assert.equal(dbRes[0].report, 1);
+            try{
+                await game.reportGame(report);
+                let dbRes = await game.getGameById(testGameId);
+                assert.equal(dbRes[0].report, 1);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('should has comment',async function() {
-            let dbRes = await game.reportComments([testGameId]);
-            assert.equal(dbRes[0].permlink, 'report http');
+            try{
+                let dbRes = await game.reportComments([testGameId]);
+                assert.equal(dbRes[0].permlink, 'report http');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('can report',async function() {
-            let dbRes = await game.canReportGame([testUserId,testGameId]);
-            assert.equal(dbRes.length, 1);
+            try{
+                let dbRes = await game.canReportGame([testUserId,testGameId]);
+                assert.equal(dbRes.length, 1);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
     describe('#activity', function() {
         it('should activity',async function() {
             let unix = Math.floor(new Date() / 1000) - 86400*8;
             let activity = {userid:testUserId, account:testAccount, gameid:testGameId, lastModified:unix, permlink:'activity'};
-            await game.addActivity(activity);
-            let dbRes = await game.getActivitiesById(testGameId);
-            assert.equal(dbRes[0].permlink, 'activity');
+            try{
+                await game.addActivity(activity);
+                let dbRes = await game.getActivitiesById(testGameId);
+                assert.equal(dbRes[0].permlink, 'activity');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('should recently activity',async function() {
-            let dbRes = await game.getRecentlyActivity(testGameId);
-            assert.equal(dbRes[0].permlink, 'activity');
+            try{
+                let dbRes = await game.getRecentlyActivity(testGameId);
+                assert.equal(dbRes[0].permlink, 'activity');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('should payout activity',async function() {
+            try{
             let dbRes = await game.getPayoutActivities(testGameId);
             assert.equal(dbRes[0].permlink, 'activity');
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
 });
 
 describe('Query', function() {
     before(async function() {
+        await game.clearCache();
         let unix = Math.round(+new Date()/1000);
         for(let i=0; i<100; i++) {
             let gameInfo = { userid:testUserId,
@@ -188,12 +246,20 @@ describe('Query', function() {
                 status:Math.floor(Math.random() * Math.floor(3)),
                 report:Math.floor(Math.random() * Math.floor(2))
             };
+            try{
             let dbRes = await game.addGame(gameInfo);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         }
     });
     after(async function() {
-        await db.execute(db.WRITE, 'delete from games where userid= ?', [testUserId]);
-        await game.clearCache();
+        try{
+            await db.execute(db.WRITE, 'delete from games where userid= ?', [testUserId]);
+            await game.clearCache();
+        } catch (err) {
+            assert.fail('test failed');
+        }
     });
     describe('#game nums', function() {
         it('should return nums ',async function() {
@@ -203,11 +269,15 @@ describe('Query', function() {
             params['recommend'] = 0;
             params['account'] = '';
             params['category'] = '';
-            let dbRes = await game.countOfGames(params);
-            let key = 'game:count:s:'+params['status']+':r:'+params['report']+':remm:'+params['recommend']+':a:'+params['account']+':c:'+params['category'];
-            let rows = await redis.instance.get(key);
-            let assertRes = JSON.parse(rows);
-            assert.equal(dbRes[0].nums, assertRes[0].nums);
+            try{
+                let dbRes = await game.countOfGames(params);
+                let key = 'game:count:s:'+params['status']+':r:'+params['report']+':remm:'+params['recommend']+':a:'+params['account']+':c:'+params['category'];
+                let rows = await redis.instance.get(key);
+                let assertRes = JSON.parse(rows);
+                assert.equal(dbRes[0].nums, assertRes[0].nums);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
 
@@ -224,15 +294,23 @@ describe('Query', function() {
         let key = 'game:list:s:'+params['status']+':r:'+params['report']+':remm:'+params['recommend']+':a:'+params['account']+':c:'+params['category']+':o:'+ params['offset']+':p:'+params['pageSize']+':s:'+params['sort'];
 
         it('should return games ',async function() {
-            let dbRes = await game.gameList(params);
-            let rows = await redis.instance.get(key);
-            let assertRes = JSON.parse(rows);
-            assert.equal(dbRes[0].id, assertRes[0].id);
+            try{
+                let dbRes = await game.gameList(params);
+                let rows = await redis.instance.get(key);
+                let assertRes = JSON.parse(rows);
+                assert.equal(dbRes[0].id, assertRes[0].id);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
         it('should return null ',async function() {
-            await game.clearCache();
-            let rows = await redis.instance.get(key);
-            assert.equal(rows, null);
+            try{
+                await game.clearCache();
+                let rows = await redis.instance.get(key);
+                assert.equal(rows, null);
+            } catch (err) {
+                assert.fail('test failed');
+            }
         });
     });
 
@@ -250,9 +328,13 @@ describe('Query', function() {
                 params['pageSize'] = 2;
                 params['sort'] = 'created_desc';
                 let key = 'game:list:s:'+params['status']+':r:'+params['report']+':remm:'+params['recommend']+':a:'+params['account']+':c:'+params['category']+':o:'+ params['offset']+':p:'+params['pageSize']+':s:'+params['sort'];
-                let dbRes = await game.gameList(params);
-                if(assertListKey.indexOf(key)===-1 && dbRes.length>0){
-                    assertListKey.push(key);
+                try{
+                    let dbRes = await game.gameList(params);
+                    if(assertListKey.indexOf(key)===-1 && dbRes.length>0){
+                        assertListKey.push(key);
+                    }
+                } catch (err) {
+                    assert.fail('test failed');
                 }
             }
             let allKey = 'game:list';
@@ -272,9 +354,13 @@ describe('Query', function() {
                 params['pageSize'] = 2;
                 params['sort'] = 'created_desc';
                 let countKey = 'game:count:s:'+params['status']+':r:'+params['report']+':remm:'+params['recommend']+':a:'+params['account']+':c:'+params['category'];
-                let dbRes = await game.countOfGames(params);
-                if(assertCountsListKey.indexOf(countKey)===-1 && dbRes[0]['nums']>0){
-                    assertCountsListKey.push(countKey);
+                try{
+                    let dbRes = await game.countOfGames(params);
+                    if(assertCountsListKey.indexOf(countKey)===-1 && dbRes[0]['nums']>0){
+                        assertCountsListKey.push(countKey);
+                    }
+                } catch (err) {
+                    assert.fail('test failed'+err);
                 }
             }
             let allKey = 'game:count';
