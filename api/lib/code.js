be426a034d9e342a76fc799890d32add91a3e88d
bonjovis <bonjovis1985@gmail.com>
2018-04-28 10:34:23 +0800

    unreport game

--- api/lib/code.js
+++ api/lib/code.js
@@ -25,6 +25,7 @@ const CODE = {
     NOFOUND_GAME_ERROR: {RESCODE:502,DESC:'game not found'},
     NOFOUND_ACTIVITY_ERROR: {RESCODE:503,DESC:'activity not found'},
     HAS_REPORT_ERROR: {RESCODE:504,DESC:'you have reported this game'},
+    NO_REPORT_ERROR: {RESCODE:504,DESC:'you have not reported this game'},
 
     //other
     TEST_DATA_ERROR: {RESCODE:700,DESC:'get test data failed'},
