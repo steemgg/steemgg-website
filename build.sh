557e869b7500c94393dc60ab18ee6379f9b7ac19
Ye Hui <bonjovis1985@gmail.com>
2018-06-15 06:31:25 +0000

    deploy ui code

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd ui
 npm install
 cd node_modules/webpack/node_modules
@@ -6,8 +8,18 @@ cd ../../../
 npm run build-prod
 cd ../
 tar -cvf  ui.tar.gz ui/dist/
+git clone https://github.com/steemgg/ui-dev.git
+cp -rf  ui/dist ui-dev
+cd ui-dev
+git commit -a -c "commit"
+git push
 rm -rf ui/dist/
 cd ui
 npm run build-staging
 cd ../
 tar -cvf  ui-staging.tar.gz ui/dist/
+git clone https://github.com/steemgg/ui-staging.git
+cp -rf  ui/dist ui-staging
+cd ui-dev
+git commit -a -c "commit"
+git push
