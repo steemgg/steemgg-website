03e3c47b026d6a76c168c70645f758a6486ea8ec
Ye Hui <bonjovis1985@gmail.com>
2018-06-19 02:14:57 +0000

    deploy ui code

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@ npm install
 cd node_modules/webpack/node_modules
 npm install uglifyjs-webpack-plugin@1.1.8
 cd ../../../
-npm run build-prod
+npm run build-dev
 cd ../
-tar -cvf  ui-prod.tar.gz ui/dist/
+#tar -cvf  ui-prod.tar.gz ui/dist/
 git clone https://github.com/steemgg/ui-dev.git
 cp -rf  ui/dist/* ui-dev
 cd ui-dev
 git config user.name "bonjovis"
 git config user.email "bonjovis1985@gmail.com"
+git commit add .
 git commit -a -m "commit"
 git push --force --quiet "https://${GH_TOKEN}@github.com/steemgg/ui-dev.git"
 cd ../
@@ -20,12 +21,13 @@ rm -rf ui/dist/
 cd ui
 npm run build-staging
 cd ../
-tar -cvf  ui-staging.tar.gz ui/dist/
+#tar -cvf  ui-staging.tar.gz ui/dist/
 git clone https://github.com/steemgg/ui-staging.git
 cp -rf  ui/dist/* ui-staging
 cd ui-staging
 git config user.name "bonjovis"
 git config user.email "bonjovis1985@gmail.com"
+git commit add .
 git commit -a -m "commit"
 git push --force --quiet "https://${GH_TOKEN}@github.com/steemgg/ui-staging.git"
 cd ../
