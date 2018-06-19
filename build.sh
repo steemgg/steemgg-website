20f4097c88b9b9ddb55d898f63e496a30d327216
Ye Hui <bonjovis1985@gmail.com>
2018-06-19 08:25:16 +0000

    travis config

--- build.sh
+++ build.sh
@@ -38,3 +38,21 @@ elif [[ $TRAVIS_BRANCH == 'master' ]]; then
 else
     echo 'helloworld'
 fi
+if [ -z $TRAVIS_TAG ]; then
+    cd ui
+    npm install
+    cd node_modules/webpack/node_modules
+    npm install uglifyjs-webpack-plugin@1.1.8
+    cd ../../../
+    npm run build-prod
+    cd ../
+    tar -cvf  release_$TRAVIS_TAG.tar.gz ui/dist/
+    git clone https://github.com/steemgg/ui-prod.git
+    cp -rf  ui/dist/* ui-prod
+    cd ui-prod
+    git config user.name "bonjovis"
+    git config user.email "bonjovis1985@gmail.com"
+    git add .
+    git commit -a -m "commit"
+    git push --force --quiet "https://${GH_TOKEN}@github.com/steemgg/ui-prod.git"
+fi
