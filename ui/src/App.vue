073f4152aefb865b0e73d663b217f7f1e70ad0ce
Erik BI <erikzbi@au1.ibm.com>
2018-07-06 19:41:53 +1000

    #153

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="pageDynamicWidth">
     <router-view/>
   </div>
 </template>
@@ -11,14 +11,17 @@ export default {
 </script>
 
 <style>
+
+
 #app {
   font-family: 'Roboto', sans-serif;
   font-size: 1em;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  width:1200px;
+  /*width: 350px;*/
   margin-left: auto;
   margin-right: auto;
 }
+
 </style>
