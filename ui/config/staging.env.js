696c20a6206d41cd4e82ba7048aa3d82581227c9
Ye Hui <bonjovis1985@gmail.com>
2018-07-25 01:20:17 +0000

    staging ipfs base url

--- ui/config/staging.env.js
+++ ui/config/staging.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"staging"',
   API_SERVER_URL: '"https://api-staging.steemgg.com/"',
   APP_ID: '"steemitgame.app"',
-  IPFS_SERVER_URL: '"http://stage-ipfs.steemgg.com:8080/ipfs/"'
+  IPFS_SERVER_URL: '"https://stage-hk-ipfs.steemgg.com/ipfs/"'
 }
