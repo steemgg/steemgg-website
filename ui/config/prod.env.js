72aa29e97fa572d7a2159f57c894e1c623c89955
Ye Hui <bonjovis1985@gmail.com>
2018-07-30 09:55:29 +0000

    change api production address

--- ui/config/prod.env.js
+++ ui/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_SERVER_URL: '"https://api-dev.steemgg.com/"',
+  API_SERVER_URL: '"https://api.steemgg.com/"',
   APP_ID: '"steemgg.app"',
   IPFS_SERVER_URL: '"https://ipfs.steemgg.com/ipfs/"'
 }
