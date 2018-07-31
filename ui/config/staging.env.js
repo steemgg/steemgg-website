4492133b28644b944294b7ae9e1148425d8ef18f
Ye Hui <bonjovis1985@gmail.com>
2018-07-31 08:32:46 +0000

    change staging api config

--- ui/config/staging.env.js
+++ ui/config/staging.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"staging"',
-  API_SERVER_URL: '"https://api-staging.steemgg.com/"',
+  API_SERVER_URL: '"https://stage-hk-api.steemgg.com/"',
   APP_ID: '"steemitgame.app"',
   IPFS_SERVER_URL: '"https://stage-hk-ipfs.steemgg.com/ipfs/"'
 }
