5b6312a18aab0e6c2a16c9c89faefd8ad82f18fa
Erik BI <erikzbi@au1.ibm.com>
2018-09-03 22:39:47 +1000

    #265

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -6,7 +6,26 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: {},
+  metaInfo: {
+    title: 'steemGG STEEM blockchain HTML5 game publishing distribution platform',
+    titleTemplate: '%s ← steemGG STEEM blockchain HTML5 game',
+    meta: [
+      {
+        'http-equiv': 'Content-Type',
+        content: 'text/html; charset=utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        name: 'description',
+        content: 'SteemGG is STEEM blockchain based HTML5 Game distribution platform, allowing Game Developers to gain monetary rewards directly from Gamers\' upvotes and likes. In return Gamers will receive rewards for actively upvoting games and posting game related contents.'
+      }
+    ]
+  }
 }
 </script>
 
